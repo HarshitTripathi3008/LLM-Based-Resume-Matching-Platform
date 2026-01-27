de7ccf64fd9ddc4d0fb151379bd8d2d175d5ba14
HarshitTripathi3008 <148792226+HarshitTripathi3008@users.noreply.github.com>
2026-01-27 14:39:31 +0530

    Remove duplicate imports in resumeController

--- backend-node/src/controllers/resumeController.js
+++ backend-node/src/controllers/resumeController.js
@@ -3,8 +3,7 @@ const path = require('path');
 const axios = require('axios');
 const fs = require('fs');
 
-const axios = require('axios');
-const fs = require('fs');
+
 
 // AI Service URL (For production, set in Env)
 let AI_SERVICE_URL = process.env.AI_SERVICE_URL || 'http://127.0.0.1:8000';
